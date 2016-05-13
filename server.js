be637a8e8181baea76e581be4014229091d4f6a0
流星雨 <zhangyu@hunlidaheng.com>
2016-05-13 17:07:49 +0800

    ..

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var http=require("http");
-
-function start(){
+var url=require("url");
+var querystring=require("querystring");
+function start(route,handle){
 	http.createServer(function(req,res){
-	 console.log("Request received.");
+		console.log('url:'+req.url);
+		var pathname=url.parse(req.url).pathname;
+	 // console.log("pathname:"+url.parse(req.url).pathname);
+	 // console.log("query:"+url.parse(req.url).query);
+	 console.log("name:"+querystring.parse(url.parse(req.url).query).name);
+	 route(handle,pathname);
+
 	res.writeHead('200',{"Content-Type":"text/plain"});
 	res.end("Hellow World!");
 }).listen(8000);
@@ -10,4 +17,4 @@ function start(){
 }
 
 console.log('server start up');
-module.exports=start;
+module.exports.start=start;
